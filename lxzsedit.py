60be1e4b0ea3c10afcd166a489d776e5c4a387da
shikeyu <15691157@qq.com>
2025-10-31 14:54:14 +0800

    Update lxzsedit.py

--- lxzsedit.py
+++ lxzsedit.py
@@ -649,7 +649,7 @@ def edit_page():
     b_up,b_down,b_input_id,b_goto=st.sidebar.columns(4, gap="small")
     button_up=b_up.button("◀️", help="上一条记录")
     button_down=b_down.button("▶️", help="下一条记录")
-    input_id=b_input_id.number_input("", min_value=0, max_value=len(ids) - 1, value="min",label_visibility="collapsed")
+    input_id=b_input_id.number_input("", min_value=0, max_value=len(ids) - 1, value=0, label_visibility="collapsed")
     button_goto=b_goto.button("🎯", help="跳转")
     
     # 跳转到指定记录
@@ -841,5 +841,6 @@ def main():
         table_selection_page()
 if __name__ == '__main__':
     main()
+
 
 
